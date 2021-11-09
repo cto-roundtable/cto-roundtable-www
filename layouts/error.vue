47b4bfe3abda0bf7842bc792a434268f0ab73e06
Johannes Berggren <johannes@berggren.co>
2021-11-09 14:22:56 +0100

    Initial Nuxt with placeholder design

---
+++ layouts/error.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app dark>
+    <h1 v-if="error.statusCode === 404">
+      {{ pageNotFound }}
+    </h1>
+    <h1 v-else>
+      {{ otherError }}
+    </h1>
+    <NuxtLink to="/">
+      Home page
+    </NuxtLink>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Error',
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  head () {
+    const title = this.error.statusCode === 404 ? '404 Not Found' : 'An error occurred'
+    return {
+      title
+    }
+  }
+})
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-img
+      src="hero.jpeg"
+      style="height: 100vh"
+    >
+      <v-row
+        justify="center"
+        align="center"
+        style="height: 100%"
+      >
+        <v-col
+          cols="12"
+          sm="8"
+        >
+          <v-card
+            class="pa-8 text-center"
+            flat
+            height="60vh"
+          >
+            <h1>Welcome to CTO Roundtable</h1>
+
+            <h4>
+              Contact us at post@ctoroundtable.no
+            </h4>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-img>
+  </div>
+</template>
