e7fcd9696ebfb050c074086086af785f68890353
socks <somesocks@gmail.com>
2021-11-19 01:22:20 +0100

    - adding new background - changing layout - adding custom body font size - fixing logo-white fill color

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,64 @@
 <template>
   <div>
     <v-img
-      src="hero.jpeg"
-      style="height: 100vh"
+      src="coding-1.jpg"
+      style="height: 100vh;"
+      gradient="rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)"
     >
-      <v-row
-        justify="center"
-        align="center"
+      <v-container
         style="height: 100%"
+      
       >
-        <v-col
-          cols="12"
-          sm="8"
+        <v-row
+          justify="center"
+          align="center"
+          style="height: 100%"
         >
-          <v-card
-            class="pa-8 text-center"
-            flat
-            height="60vh"
-          >
-            <h1 class="text-h4 text-uppercase">CTO Roundtable</h1>
+          <v-col cols="12 xs="12>
+            <v-container
+            >
+              <v-row
+              >
+                <v-col cols="12" xs="0" sm="1" />
+                <v-col
+                  cols="12"
+                  sm="10"
+                >
+                  <h1 class="text-h2 font-weight-medium white--text" style="line-height:1.25">
+                    There's a knowledge gap in your startup.<br />We want to close it.
+                  </h1>
+                </v-col>
+                <v-col cols="12" xs="0" sm="1" />
+                <v-col cols="12" xs="0" sm="1" />
+                <v-col
+                  cols="12"
+                  sm="5"
+                >
+                  <p class="text-body  font-weight-medium white--text">
+                    <br /><br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper viverra nam libero justo laoreet. Nisl rhoncus mattis rhoncus urna neque viverra. Nec feugiat in fermentum posuere urna. Felis donec et odio pellentesque diam volutpat commodo sed egestas.
+                    <br /><br />
+                    Vel risus commodo viverra maecenas accumsan lacus vel facilisis. Turpis egestas maecenas pharetra convallis posuere morbi leo urna molestie. Feugiat nisl pretium fusce id velit. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor. Cras fermentum odio eu feugiat pretium nibh ipsum.          
+                  </p>
+                  <br /><br />
 
-            <h4 class="text-subtitle-1">
-              Contact us at <a href="mailto:post@ctoroundtable.no">post@ctoroundtable.no</a>
-            </h4>
-          </v-card>
-        </v-col>
-      </v-row>
+                  <h4 class="font-weight-medium text-body white--text">
+                    Contact us at <a class="white--text" href="mailto:post@ctoroundtable.no">post@ctoroundtable.no</a>
+                  </h4>
+
+                  <br /><br />
+                  <v-img
+                    src="logo-white.svg"
+                    style="max-width:240px;"
+                  />
+
+                </v-col>
+                <v-col cols="12" xs="0" sm="6" />
+              </v-row>
+            </v-container>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-img>
   </div>
 </template>
