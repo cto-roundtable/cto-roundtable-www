8b3c8874166cf5ed4e018cc1fc1be4d2d2e7020f
socks <somesocks@gmail.com>
2021-12-09 23:39:54 +0100

    - testing a different fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,24 @@
 <template>
-  <div style="background-color: #111;" v-cloak>
+  <div style="background-color: #111;">
     <v-img
       src="coding-1.jpg"
       style="height: 100vh;"
       gradient="rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)"
     >
-      <v-container
-        v-cloak
-        fluid
-        style="width: 100%; height: 100%; overflow-y: auto;"      
-      >
-        <v-row
-          justify="center"
-          align="center"
-          style="height: 100%"
-        >
-          <v-col cols="12 xs="12>
-            <v-container
-            >
-              <v-row
-              >
-                <v-col cols="12" xs="0" sm="1" />
-                <v-col
-                  cols="12"
-                  sm="10"
-                >
-                  <h1 class="text-h4 text-md-h3 font-weight-medium white--text" style="line-height:1.5">
-                    There's a knowledge gap in your startup.
-                    <br />
-                    We want to close it.
-                  </h1>
-                </v-col>
-                <v-col cols="12" xs="0" sm="1" />
-                <v-col cols="12" xs="0" sm="1" />
-                <v-col
-                  cols="12"
-                  sm="10"
-                  lg="5"
-                >
-                  <p class="text-body font-weight-normal white--text" style="line-height:1.5">
-                  <br />
-                  Ideas are cheap, execution is everything.  But without the right knowledge, execution can be an uphill battle.  We believe that funding should come together with knowledge, with the insights needed to build a successful product and scale.  
-                  <br /><br />
-                  That's why we formed CTO Roundtable Invest, to provide a combination of funding and experience that we see as critical for startups.  We're looking for early-stage companies to invest in.
-                  </p>
-                  <br />
-
-                  <h4 class="font-weight-normal text-body white--text">
-                    Contact us at <a class="white--text" href="mailto:post@ctoroundtable.no">post@ctoroundtable.no</a>
-                  </h4>
-
-                  <br /><br />
-                  <v-img
-                    src="logo-white.svg"
-                    style="max-width:240px;"
-                  />
-
-                </v-col>
-                <v-col cols="12" xs="0" sm="6" />
-              </v-row>
-            </v-container>
-          </v-col>
-        </v-row>
-      </v-container>
+      <IndexBody v-cloak />
     </v-img>
   </div>
 </template>
+
+<script setup>
+  import IndexBody from './index-body';
+
+  export default {
+    components: {
+      IndexBody,
+    },
+  };
+</script>
 
 <style scoped>
 [v-cloak] {
