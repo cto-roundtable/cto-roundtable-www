3e2cad47dc6a4e7bd292871afd7573846bc96772
Johannes Berggren <jb@learnlink.no>
2022-05-15 12:13:11 +0200

    Add members component.

--- pages/index.vue
+++ pages/index.vue
@@ -7,58 +7,59 @@
     >
       <v-container
         fluid
-        style="width: 100%; height: 100%; overflow-y: auto;"      
+        style="width: 100%; height: 100%; overflow-y: auto;"
+        class="pa-12"
       >
-        <v-row
-          justify="center"
-          align="center"
-          style="height: 100%"
-        >
-          <v-col cols="12 xs="12>
-            <v-container
+        <v-row>
+          <v-col cols="12" md="8">
+            <v-row
+              style="position: sticky; top: 10px"
             >
-              <v-row
+              <v-col
+                cols="12"
+                sm="10"
               >
-                <v-col cols="12" xs="0" sm="1" />
-                <v-col
-                  cols="12"
-                  sm="10"
-                >
-                  <h1 class="text-h4 text-md-h3 font-weight-medium white--text" style="line-height:1.5">
-                    There's a knowledge gap in your startup.
-                    <br />
-                    We want to close it.
-                  </h1>
-                </v-col>
-                <v-col cols="12" xs="0" sm="1" />
-                <v-col cols="12" xs="0" sm="1" />
-                <v-col
-                  cols="12"
-                  sm="10"
-                  lg="5"
-                >
-                  <p class="text-body font-weight-normal white--text" style="line-height:1.5">
-                  <br />
-                  Ideas are cheap, execution is everything.  But without the right knowledge, execution can be an uphill battle.  We believe that funding should come together with knowledge, with the insights needed to build a successful product and scale.  
-                  <br /><br />
-                  That's why we formed CTO Roundtable Invest, to provide a combination of funding and experience that we see as critical for startups.  We're looking for early-stage companies to invest in.
-                  </p>
-                  <br />
+                <h1 class="text-h4 text-md-h3 font-weight-medium white--text" style="line-height:1.5">
+                  Ideas are cheap,
+                  <br>
+                  execution is everything.
+                </h1>
+              </v-col>
+              <v-col cols="12" xs="0" sm="1" />
+              <v-col cols="12" xs="0" sm="1" />
+              <v-col
+                cols="12"
+                sm="10"
+                lg="8"
+              >
+                <p class="text-body font-weight-normal white--text" style="line-height:1.5">
+                  Execution can be an uphill battle.<br>We believe funding should come with the knowledge and experience
+                  needed to build and scale a successful product.
+                  <br><br>
+                  That's why we formed CTO Roundtable Invest, to provide a combination of funding and experience that
+                  we see as critical for startups. We're looking for early-stage companies to invest in.
+                </p>
+                <br>
 
-                  <h4 class="font-weight-normal text-body white--text">
-                    Contact us at <a class="white--text" href="mailto:post@ctoroundtable.no">post@ctoroundtable.no</a>
-                  </h4>
+                <h4 class="font-weight-normal text-body white--text">
+                  Contact us at <a class="white--text" href="mailto:post@ctoroundtable.no">post@ctoroundtable.no</a>
+                </h4>
 
-                  <br /><br />
-                  <v-img
-                    src="logo-white.svg"
-                    style="max-width:240px;"
-                  />
+                <br><br>
+                <v-img
+                  src="logo-white.svg"
+                  style="max-width:240px;"
+                />
+              </v-col>
+              <v-col cols="12" xs="0" sm="6" />
+            </v-row>
+          </v-col>
 
-                </v-col>
-                <v-col cols="12" xs="0" sm="6" />
-              </v-row>
-            </v-container>
+          <v-col cols="12" md="4">
+            <h3 class="white--text text-center mt-10 mb-3">
+              CTO Roundtable Investors
+            </h3>
+            <members />
           </v-col>
         </v-row>
       </v-container>
@@ -66,3 +67,10 @@
   </div>
 </template>
 
+<script lang="ts">
+import Members from '~/pages/members.vue'
+
+export default {
+  components: { Members }
+}
+</script>
